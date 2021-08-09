40640ab1248f227229383fdc6f14c5f49d59b369
kuriatsu <kuriatsubayashi712@gmail.com>
2021-08-09 19:32:09 +0900

    intervene for obstacle not for curve

--- auto_intervention.py
+++ auto_intervention.py
@@ -10,17 +10,18 @@ import numpy as np
 from autoware_msgs.msg import VehicleCmd
 from geometry_msgs.msg import PoseStamped, Point32, Twist
 from sensor_msgs.msg import PointCloud, ChannelFloat32
-from std_msgs.msg import Int16, Bool
+from std_msgs.msg import Int32, Bool
+from visualization_msgs.msg import MarkerArray, Marker
 import tf
 
 class AutoIntervention():
 
     def __init__(self, load_data_file, intervention):
-        self.intervention = Bool(intervention)
-        self.intervened = False
+        self.intervention = bool(int(intervention))
         self.current_data_index = 0
         self.current_pose = None
         self.time_step_data = []
+        self.obstacle_stop = False
 
         with open(load_data_file, 'rb') as f:
             self.data = pickle.load(f)
@@ -30,9 +31,10 @@ class AutoIntervention():
         self.pub_twist = rospy.Publisher('/vehicle_cmd', VehicleCmd, queue_size=1)
         self.pub_intervention = rospy.Publisher('/is_intervened', Bool, queue_size=1)
         self.sub_twist = rospy.Subscriber('/vehicle_cmd_autoware', VehicleCmd, self.twistCb)
-        self.sub_current_scenario = rospy.Subscriber('/current_scenario', Int16, self.currentScenarioCb)
+        self.sub_current_scenario = rospy.Subscriber('/current_scenario', Int32, self.currentScenarioCb)
+        self.sub_detection_range = rospy.Subscriber('/detection_range', MarkerArray, self.detectionRangeCb)
         # rospy.Timer(rospy.Duration(0.5), self.storeData)
-
+        print(self.intervention)
 
     # def quatToYaw(orientation):
     #     euler = tf.transformations.euler_from_quaternion((orientation.x, orientation.y, orientation.z, orientation.w))
@@ -41,25 +43,54 @@ class AutoIntervention():
     def currentScenarioCb(self, msg):
         self.current_data_index = msg.data
 
+    def detectionRangeCb(self, msg):
+
+        self.obstacle_stop = False
+
+        for marker in msg.markers:
+            if marker.ns == 'Stop Line':
+                self.obstacle_stop = True
+
+
     def twistCb(self, msg):
 
-        intervention_thres = 5.0 / 3.6
+        vel_range = 0.5 # rate
+        curve_vel = 8.0 # m/s
+        avoid_stop_vel = 4.0
+        angular_multiply_rate = 4.0
+        curve_angular = 0.1
+
         carla_speed = self.data[self.current_data_index].get('waypoint')[4]
         autoware_speed = msg.twist_cmd.twist.linear.x
-        out_twist = VehicleCmd()
-        if (autoware_speed > carla_speed + intervention_thres) or (autoware_speed < 10.0/3.6) and self.intervention:
-        # if carla_speed - intervention_thres < autoware_speed < carla_speed + intervention_thres:
-            out_twist = msg
-            out_twist.twist_cmd.twist.linear.x = carla_speed
-            out_twist.twist_cmd.twist.angular.z *= 2 * carla_speed / autoware_speed
-            self.pub_intervention.publish(Bool(data=True))
-            print(self.data[self.current_data_index].get('speed_limit'), carla_speed / autoware_speed, 'true')
+        out_twist = msg
+        out_twist.twist_cmd.twist.angular.z *= angular_rate
+
+        if self.intervention:
+            if autoware_speed > carla_speed * (1.0 + vel_range):
+                print('brake (over speed)')
+                out_twist.twist_cmd.twist.linear.x = carla_speed
+                self.pub_intervention.publish(Bool(data=True))
+
+            elif autoware_speed < carla_speed * (1.0 - vel_range) and self.obstacle_stop:
+                if abs(out_twist.twist_cmd.twist.angular.z) < curve_angular:
+                    print('straight obstacle accel')
+                    out_twist.twist_cmd.twist.linear.x = carla_speed
+                    self.pub_intervention.publish(Bool(data=True))
+
+                else:
+                    print('curve obstacle accel')
+                    out_twist.twist_cmd.twist.linear.x = curve_vel
+                    self.pub_intervention.publish(Bool(data=True))
+
+            if autoware_speed == 0.0 and carla_speed == 0.0:
+                print('avoid stop')
+                out_twist.twist_cmd.twist.linear.x = avoid_stop_vel
+                self.pub_intervention.publish(Bool(data=False))
+
         else:
-            out_twist = msg
             self.pub_intervention.publish(Bool(data=False))
-            print(self.data[self.current_data_index].get('speed_limit'), carla_speed, autoware_speed, 'false')
 
-
+        print(out_twist.twist_cmd.twist.angular.z, autoware_speed, carla_speed)
         self.pub_twist.publish(out_twist)
 
 
