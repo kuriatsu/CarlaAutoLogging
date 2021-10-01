f66ce99ab99419f0cf5d173eec426d0ab8c16dce
kuriatsu <kuriatsubayashi712@gmail.com>
2021-10-01 20:32:44 +0900

    before debug

--- collect_ros_data/intervention.py
+++ collect_ros_data/intervention.py
@@ -7,7 +7,7 @@ import pickle
 import sys
 import numpy as np
 
-from autoware_msgs.msg import VehicleCmd
+from autoware_msgs.msg import VehicleCmd, DetectedObjectArray
 from geometry_msgs.msg import PoseStamped, Point32, Twist, TwistStamped, PoseWithCovarianceStamped
 from sensor_msgs.msg import PointCloud, ChannelFloat32
 from std_msgs.msg import Int32, Bool, Float32, String
@@ -22,21 +22,29 @@ class AutoIntervention():
         self.time_step_data = []
         self.is_obstacle_on_path = False
         self.carla_speed = None
-        self.get_carla_speed = False
-        self.max_intervention_count = 1
+
         self.intervention_count = 0
         self.last_intervention_time = None
         self.last_intervention_mileage = None
-        self.start_intervention_time = 33.0
         self.allow_intervention = False
         self.intervention_gap = 1.0
-        self.current_mileage = 0.0
         self.progress_as_succeed = 90.0
+        self.current_mileage = 0.0
+
+        # remove detection noise
         self.last_obstacle_time = None
         self.obstacle_detection_gap = 0.5
 
-        f = open("last_int_distance.txt", "r")
-        self.start_intervention_mileage = float(f.read())
+        # intervention control
+        self.start_intervention_time = 33.0
+        self.current_progress = 0.0
+        self.max_intervention_count = 1
+        self.objects = None
+        self.intervention_target = []
+        self.obstacle = None
+
+        f = open("intervention_target.txt", "r")
+        self.intervention_target_list = f.read().split(',')
         f.close()
 
         self.pub_twist = rospy.Publisher('/vehicle_cmd', VehicleCmd, queue_size=1)
@@ -48,6 +56,8 @@ class AutoIntervention():
         self.sub_initial_pose = rospy.Subscriber('/initialpose', PoseWithCovarianceStamped, self.initialposeCb)
         self.sub_simulate_progress = rospy.Subscriber('/simulate_progress', Float32, self.simulateProgressCb)
         self.sub_mileage_progress = rospy.Subscriber('/mileage_progress', Float32, self.mileageProgressCb)
+        self.sub_obstacle = rospy.Subscriber('/obstacle', Marker, self.obstacleCb)
+        self.sub_objects = rospy.Subscriber('/detection/contour_tracker/objects', DetectedObjectArray, self.objectsCb)
 
     def carlaSpeedCb(self, msg):
         self.carla_speed = msg.data
@@ -56,6 +66,7 @@ class AutoIntervention():
     def detectionRangeCb(self, msg):
 
         self.is_obstacle_on_path = False
+        # remove noise
         if self.last_obstacle_time is not None:
             if rospy.Time.now() - self.last_obstacle_time < rospy.Duration(self.obstacle_detection_gap):
                 self.is_obstacle_on_path = True
@@ -64,6 +75,24 @@ class AutoIntervention():
             if marker.ns == 'Stop Line' and marker.color.g == 1.0: # for deceleration object
                 self.is_obstacle_on_path = True
                 self.last_obstacle_time = rospy.Time.now()
+
+    def obstacleCb(self, msg):
+        if self.objects is None:
+            return
+
+        self.obstacle = findClosestObject(msg.pose, self.objects)
+
+    def findClosestObject(self, pose, objects):
+        min_dist = 1000000
+        closest_object = None
+        for object in objects:
+            dist = (pose.position.x - object.pose.position.x)**2 + (pose.position.y - object.pose.position.y)**2
+            if min_dist > dist:
+                min_dist = dist
+                closest_object = object
+
+    def objectsCb(self, msg):
+        self.objects = msg.objects
 
     def simulateProgressCb(self, msg):
         self.current_progress = msg.data
@@ -85,12 +114,11 @@ class AutoIntervention():
         out_twist = VehicleCmd()
         out_twist.twist_cmd.twist = msg.twist
 
-        if self.last_intervention_time is not None:
-            if rospy.Time.now() - self.last_intervention_time > rospy.Duration(self.intervention_gap):
-                self.intervention_count += 1
-                self.last_intervention_time = None
+        self.allow_intervention = self.is_intervention_trial and \
+                                  self.current_progress > self.start_intervention_time and \
+                                  self.intervention_count < len(self.intervention_target) and \
+                                  self.obstacle not in self.intervention_target_list
 
-        self.allow_intervention = (self.is_intervention_trial and self.current_progress > self.start_intervention_time and self.current_mileage > self.start_intervention_mileage and self.intervention_count < self.max_intervention_count)
         print("start_time: {}, start_mileage:{}, count{}".format(self.start_intervention_time, self.start_intervention_mileage, self.intervention_count))
 
         if self.allow_intervention:
@@ -98,15 +126,15 @@ class AutoIntervention():
                 self.pub_string.publish(String(data='Brake'))
                 out_twist.twist_cmd.twist.linear.x = carla_speed
                 self.pub_intervention.publish(Bool(data=True))
-                self.last_intervention_time = rospy.Time.now()
-                self.last_intervention_mileage = self.current_mileage
+                if self.obstacle.id not in self.intervention_target:
+                    self.intervention_target.append(self.obstacle.id)
 
             elif autoware_speed < carla_speed * (1.0 - vel_eps) and self.is_obstacle_on_path:
                 self.pub_string.publish(String(data='Accel'))
                 out_twist.twist_cmd.twist.linear.x = carla_speed
                 self.pub_intervention.publish(Bool(data=True))
-                self.last_intervention_time = rospy.Time.now()
-                self.last_intervention_mileage = self.current_mileage
+                if self.obstacle.id not in self.intervention_target:
+                    self.intervention_target.append(self.obstacle.id)
 
             else:
                 self.pub_string.publish(String(data=''))
@@ -133,15 +161,12 @@ class AutoIntervention():
         self.pub_twist.publish(out_twist)
 
     def log_int_mileage(self):
-        if self.last_intervention_mileage is not None:
-            print("write last intervention mileage")
-            with open("last_int_distance.txt", "w") as f:
-                f.write(str(self.last_intervention_mileage))
-
-        elif self.current_progress > self.progress_as_succeed:
-            print("no intervention and finished")
-            with open("last_int_distance.txt", "w") as f:
-                f.write(str(0.0))
+        if not self.intervention_target:
+            return
+            
+        out_list = self.intervention_target_list + [str(v) for v in self.intervention_target]
+        with open("last_int_distance.txt", "w") as f:
+            f.write(out_list)
 
 
 if __name__ == '__main__':
